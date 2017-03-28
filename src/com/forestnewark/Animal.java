3840e9e2a242371c99b93b660efb43b0dfd75f90
Forest Newark <forest.newark@gmail.com>
2017-03-28 13:38:34 -0400

    Animal class test created

--- src/com/forestnewark/Animal.java
+++ src/com/forestnewark/Animal.java
@@ -1,6 +1,6 @@
 package com.forestnewark;
 
-
+import java.time.LocalDate;
 
 /**
  * Created by Forest Newark on 3/28/17 using TDD. The Aniaml class modesl that animals that are in the shelter.
@@ -8,7 +8,61 @@ package com.forestnewark;
  */
 public class Animal {
 
+    private String name;
+    private String species;
+    private String breed;
+    private String description;
+    private LocalDate dateAdded;
 
+    public Animal(String name, String species, String breed, String description) {
+
+        this.name = name;
+        this.species = species;
+        this.breed = breed;
+        this.description = description;
+        this.dateAdded = LocalDate.now();
+    }
+
+    public Animal() {}
+
+    public void setName(String name) {
+        this.name = name;
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDateAdded(LocalDate dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public LocalDate getDateAdded() {
+        return dateAdded;
+    }
 }
-
-
--- test/com/forestnewark/AnimalTest.java
+++ test/com/forestnewark/AnimalTest.java
@@ -1,7 +1,125 @@
 package com.forestnewark;
 
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.notNullValue;
+
 /**
  * Created by forestnewark on 3/28/17.
  */
 public class AnimalTest {
+
+    @Test
+    /**
+     * animal constructor creates a non-null object
+     */
+    public void animalConstructorCreatesNonNullObject() {
+        Animal animal = new Animal();
+        assertThat(animal,notNullValue());
+    }
+
+    @Test
+    /**
+     *  animal 'name' getter and setter assign and return correct value
+     */
+    public void nameGetterAndSetterAssignAndReturnCorrectValue() {
+        Animal animal = new Animal();
+        animal.setName("Melody");
+        assertThat(animal.getName(),equalTo("Melody"));
+    }
+
+    @Test
+    /**
+     * animal 'species' getter and setter assign and return correct value
+     */
+    public void speciesGetterAndSetterAssignAndReturnCorrectValue() {
+        Animal animal = new Animal();
+        animal.setSpecies("Dog");
+        assertThat(animal.getSpecies(),equalTo("Dog"));
+    }
+
+    @Test
+    /**
+     * animal 'breed' getter and setter assign and return correct value
+     */
+    public void breedGetterAndSetterAssignAndReturnCorrectValue() {
+        Animal animal = new Animal();
+        animal.setBreed("Border Collie");
+        assertThat(animal.getBreed(),equalTo("Border Collie"));
+    }
+
+    @Test
+    /**
+     * animal 'description' getter and setter assign and return correct value
+     */
+    public void descriptionGetterAndSetterAssignAndReturnCorrectValue() {
+        Animal animal = new Animal();
+        animal.setDescription("Black and white dog that jumps a lot!");
+        assertThat(animal.getDescription(),equalTo("Black and white dog that jumps a lot!"));
+    }
+
+    @Test
+    /**
+     * animal 'dateAdded' getter and setter assign and return correct value
+     */
+    public void dateAddedGetterAndSetterAssignAndReturnCorrectValues() {
+        Animal animal = new Animal();
+        animal.setDateAdded(LocalDate.now());
+        assertThat(animal.getDateAdded(),equalTo(LocalDate.now()));
+    }
+
+    @Test
+    /**
+     * animal constructor sets correct name value
+     */
+    public void animalConstructorSetsCorrectName() {
+        Animal animal = new Animal("Melody","Dog","Border Collie","Jumps a lot");
+        assertThat(animal.getName(),equalTo("Melody"));
+    }
+
+    @Test
+    /**
+     * animal constructor sets correct species value
+     */
+    public void animalConstructorSetsCorrectSpecies(){
+        Animal animal = new Animal("Melody","Dog","Border Collie","Jumps a lot");
+        assertThat(animal.getSpecies(),equalTo("Dog"));
+    }
+
+    @Test
+    /**
+     * animal constructor sets correct breed value
+     */
+    public void animalConstructorSetsCorrectBreed() {
+        Animal animal = new Animal("Melody","Dog","Border Collie","Jumps a lot");
+        assertThat(animal.getBreed(),equalTo("Border Collie"));
+    }
+
+    @Test
+    /**
+     * animal constructor sets correct description value
+     */
+    public void animalConstructorSetsCorrectDescription() {
+        Animal animal = new Animal("Melody","Dog","Border Collie","Jumps a lot");
+        assertThat(animal.getDescription(),equalTo("Jumps a lot"));
+    }
+
+    @Test
+    /**
+     * animal constructor sets correct dateAdded value
+     */
+    public void animalConstructorSetsCorrectDateAdded() {
+        Animal animal = new Animal("Melody","Dog","Border Collie","Jumps a lot");
+        assertThat(animal.getDateAdded(),equalTo(LocalDate.now()));
+    }
+
+
+
+
+
+
 }
